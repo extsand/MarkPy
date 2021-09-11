0fc9b48f0b2c5dc82d80214b34a501cd5251f161
Alexander <extsand@gmail.com>
2021-09-11 21:51:06 +0300

    remove unuseless data

--- hello.py
+++ hello.py
@@ -2,16 +2,19 @@ welcome_msg = 'Hello, Mark! I glad to see you today'
 
 print(welcome_msg)
 
-def showName(name, text):
+def show_message(name, text='oops, I did not see text...'):
     print('======== message ===============')
     print(f'{name} say {text}')
     print('================================')
 
+msg = ''
+while msg != 'exit':
 
-name = ''
-while name != 'exit':
-    name = input('Enter your Name: \n')
-    msg = input('Enter your Text: \n')
-    showName(name=name, text=msg)
+    msg = input('Enter your name: \n')
+    text = input('Enter your text: \n')
+
+    show_message(name=msg, text=text)
+
+
 
 
